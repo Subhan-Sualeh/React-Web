129b47538ebf4eb758600c8786631328dd5cce86
Subhan-Sualeh <subhansualeh8x@gmail.com>
2022-09-01 16:44:05 +0500

    Assignment

---
+++ src/App.js
@@ -0,0 +1,155 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <section class="bgimage">
+    <div class="navbar">
+    <a href="#">
+    <li>Community</li>
+    </a>
+    <a href="#"><li>Portfolio</li></a>
+    <a href="#"><li>Inspiration</li></a>
+    <a href="#"><li>News</li></a>
+    <a href="#"><li>Feactures</li></a>
+    <a href="#"><li>Jobs</li></a>
+    </div><div class="cardbundle">
+    <section class="cardBg">
+    <div class="cards"><h3 class="p1">Services</h3><div>
+    <i class="fa-solid fa-screwdriver"></i></div>
+    <p class="p2">Lorem ipsum dolor sit amet consectetur.</p>
+    <p class="randomLine"><hr></hr></p>
+    <p class="p3"> consectetur adipisicing</p>
+    <p class="p3"> Eius corrupti!</p>
+    <p class="p4"> dolor sit</p></div>
+    </section><section class="cardBg">
+    <div class="cards"><h3 class="p1">Shared Files</h3><div><i class="fa-solid fa-file"></i></div>
+    <p class="p2">Lorem ipsum dolor sit amet consectetur.</p>
+    <p class="randomLine"><hr></hr></p>
+    <p class="p3"> consectetur adipisicing</p>
+    <p class="p3"> Eius corrupti!</p><p class="p4">
+     dolor sit</p>
+     
+     </div>
+     </section>
+     <section class="cardBg">
+     <div class="cards">
+     <h3 class="p1">Manage list and subscribers
+     </h3>
+     <div>
+     <i class="fa-solid fa-list">
+     </i>
+     </div>
+     <p class="p2">Lorem ipsum dolor sit amet consectetur.
+     </p>
+     <p class="randomLine"><hr></hr>
+     </p>
+     <p class="p3"> consectetur adipisicing
+     </p>
+     <p class="p3"> Eius corrupti!
+     </p>
+     <p class="p4"> dolor sit
+     </p>
+     </div>
+     </section>
+     <section class="cardBg">
+     <div class="cards">
+     <h3 class="p1">Videos &amp; Photos
+     </h3>
+     <div>
+     <i class="fa-solid fa-photo-film">
+     </i>
+     </div>
+     <p class="p2">Lorem ipsum dolor sit amet consectetur.
+     </p>
+     <p class="randomLine"><hr></hr>
+     </p>
+     <p class="p3"> consectetur adipisicing
+     </p>
+     <p class="p3"> Eius corrupti!
+     </p>
+     <p class="p4"> dolor sit
+     </p>
+     </div>
+     </section>
+     </div>
+     <div class="infoLine">
+     <div class="slBox">
+     <h3 class="heading">Commited To Our Client
+     </h3>
+     <div class="slIcon">
+     <i class="fa-solid fa-handshake handshake">
+     </i>
+     </div>
+     <p class="para">Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi quos pariatur saepe repellat neque, deserunt excepturi laudantium similique obcaecati assumenda omnis qui consectetur facilis eos fuga aut id aliquid iste enim nihil rerum velit modi? Nostrum perspiciatis laudantium earum, minus temporibus architecto obcaecati nam aspernatur aut, dolor quaerat! Expedita, perferendis?
+     </p>
+     </div>
+     <div class="slBox">
+     <h3 class="heading">Solution From the Bottom Line
+     </h3>
+     <div class="slIcon">
+     <i class="fa-solid fa-lightbulb">
+     </i>
+     </div>
+     <p class="para">Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi quos pariatur saepe repellat neque, deserunt excepturi laudantium similique obcaecati assumenda omnis qui consectetur facilis eos fuga aut id aliquid iste enim nihil rerum velit modi? Nostrum perspiciatis laudantium earum, minus temporibus architecto obcaecati nam aspernatur aut, dolor quaerat! Expedita, perferendis?
+     </p>
+     </div>
+     </div>
+     <div class="footerbg">
+     <section class="footer">
+     <section class="footersec">
+     <div class="mainDiv">
+     <div class="footerIcon">
+     <i class="fa-solid fa-book">
+     </i>
+     </div>
+     <div>
+     <h3>SUBSCRIBE
+     </h3>
+     <h5>GET OUR NEWSLETTER
+     </h5>
+     </div>
+     </div>
+     <p class="extra">Subscribe
+     </p>
+     </section>
+     <section class="footersec">
+     <div class="mainDiv">
+     <div class="footerIcon">
+     <i class="fa-solid fa-people-arrows">
+     </i>
+       </div>
+     <div>
+     <h3>FOLLOW US
+     </h3>
+     <h5>ON SOCIAL MEDIA
+     </h5>
+     </div>
+     </div>
+     <p class="extra">Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque numquam totam nam, sint dolores id?
+     </p>
+     </section>
+     <section class="footersec">
+     <div class="mainDiv">
+     <div class="footerIcon">
+     <i class="fa-solid fa-envelopes-bulk">
+     </i>
+     </div>
+     <div>
+     <h3>EMAIL
+     </h3>
+     <h5>CONTACT US
+     </h5>
+     </div>
+     </div>
+     <p class="extra">EMAIL:infomail@gmail.com
+     </p>
+     </section>
+     </section>
+     </div>
+     </section>
+  );
+}
+
+export default App;
+
